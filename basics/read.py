b5a59eafde4fc8e97bb0ff6630f421231d858fd7
abilcode <m.nailfad@gmail.com>
2022-10-21 07:17:52 +0700

    init

--- basics/read.py
+++ basics/read.py
@@ -4,4 +4,13 @@ import cv2 as cv
 
 capture = cv.VideoCapture('../Resources/Videos/dog.mp4')
 
+while True :
+    isTrue, frame = capture.read()
+    cv.imshow("Video",frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
+
 # cv.waitKey(0)
